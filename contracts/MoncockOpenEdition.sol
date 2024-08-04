fe3fcf2c538e47582d6b0f5cd1d42142eeba4f14
diewland <diewland@gmail.com>
2024-08-04 15:30:53 +0700

    add open edition contract

---
+++ contracts/MoncockOpenEdition.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MoncockOpenEdition is ERC721A, Ownable {
+
+    // config
+    constructor(address initialOwner) ERC721A("Moncock Open Edition", "MONCOCKOE") Ownable(initialOwner) {}
+    uint256 public MAX_MINT_PER_WALLET = 356;
+    uint256 public START_ID = 1;
+
+    bool public mintEnabled = true;
+    string public baseURI = "https://moncock.github.io/launchpad/assets/origin.png";
+
+    // start token id
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return START_ID;
+    }
+
+    // metadata
+    function setBaseURI(string calldata _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory jsonPreImage = string.concat(
+            string.concat(
+                string.concat('{"name": "Moncock Open Edition #', Strings.toString(tokenId)),
+                '","description":"We are Moncock","image":"'
+            ),
+            baseURI
+        );
+        string memory jsonPostImage = '"}';
+        return
+            string.concat(
+                "data:application/json;utf8,",
+                string.concat(jsonPreImage, jsonPostImage)
+            );
+    }
+
+    // toggle sale
+    function toggleSale() external onlyOwner {
+        mintEnabled = !mintEnabled;
+    }
+
+    // mint
+    function mint(uint quantity, bytes32[] calldata _merkleProof) external {
+        quantity = 1; // force mint single NFT
+        require(mintEnabled, "Sale is not enabled");
+        require(_numberMinted(msg.sender) + quantity <= MAX_MINT_PER_WALLET, "Over wallet limit");
+        
+        _mint(msg.sender, quantity);
+    }
+    function adminMint(uint quantity) external onlyOwner {
+        _mint(msg.sender, quantity);
+    }
+
+    // aliases
+    function numberMinted(address owner) external view returns (uint256) {
+        return _numberMinted(owner);
+    }
+    function remainingSupply() external pure returns (uint256) {
+        return 0;
+    }
+
+}
