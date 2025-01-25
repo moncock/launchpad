a219757bd3c446a13f65345db494aa11d70e4b5a
diewland <diewland@gmail.com>
2025-01-25 14:41:56 +0700

    change w1 supply to 5000

--- contracts/MoncockW1.sol
+++ contracts/MoncockW1.sol
@@ -9,7 +9,7 @@ contract MoncockW1 is ERC721A, Ownable {
 
     // config
     constructor(address initialOwner) ERC721A("Moncock W1", "MONCOCKW1") Ownable(initialOwner) {}
-    uint256 public MAX_SUPPLY = 10_000;
+    uint256 public MAX_SUPPLY = 5_000;
     uint256 public MAX_MINT_PER_WALLET = 2;
     uint256 public START_ID = 1;
 
